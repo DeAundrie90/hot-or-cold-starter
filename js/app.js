0ea6967787cdf280ff4ea8d582062e4a6f42e3b1
De'Aundrie Howard <deaundrie7@gmail.com>
2015-10-10 10:52:02 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-
 $(document).ready(function(){
+
+
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -12,6 +12,36 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  /*-- Initial Variables --*/
+    var count = 0;
+    var randNum = 0;
+    finish = false;
+    var userGuess = 0;
+
+  /*-- Random Number Generator --*/
+    function randomNumberGenerator() {
+        randNum = (Math.floor(Math.random()*100));
+        console.log("Random Number = " + randNum);
+    }
+    randomNumberGenerator(); 
+
+    /*-- Counting the number of guess --*/
+    function incrementCounter() {
+      count++;
+      $('#count').text(count);
+    };
+
+
+
+  /*-- Appending Number Guessed --*/
+    $('#guessButton').click(function(e) {
+      e.preventDefault();
+      var guess = $('#userGuess').val();
+      var list = '<li>' + guess + '</li>';
+      $('#guessList').append(list);
+      $('#userGuess').val('');
+      incrementCounter();
+    });
+
+
 });
-
-
