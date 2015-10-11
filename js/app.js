8a163c2710a9534505b462856ea26cb13a23b19f
De'Aundrie Howard <deaundrie7@gmail.com>
2015-10-11 14:46:43 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -11,37 +11,84 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+var count = 0;
+var number;
 
-  /*-- Initial Variables --*/
-    var count = 0;
-    var randNum = 0;
-    finish = false;
-    var userGuess = 0;
+function incrementCounter(){
+  count++; 
+  $('#count').text(count);
+};
 
-  /*-- Random Number Generator --*/
-    function randomNumberGenerator() {
-        randNum = (Math.floor(Math.random()*100));
-        console.log("Random Number = " + randNum);
-    }
-    randomNumberGenerator(); 
+function checkGuess(guess){
+  var diff;
+  var guessInt = parseInt(guess);
+  
+  if (guess == number){
+    $("#feedback").text("Correct!");
+  } else if (guessInt > number) {
+    diff = guessInt - number;
+  } else if (guessInt < number) {
+    diff = number - guessInt;
 
-    /*-- Counting the number of guess --*/
-    function incrementCounter() {
-      count++;
-      $('#count').text(count);
-    };
+  }
+
+  if (diff >= 1 && diff <= 10) {
+    $("#feedback").text("Very Hot");
+  } else if (diff < 10 && diff <= 20){
+    $("#feedback").text("Hot");
+  } else if (diff < 20 && diff <= 30){
+    $("#feedback").text("Warm");
+  } else if (diff < 30 && diff <= 50){
+    $("#feedback").text("Cold");
+  } else if ( diff > 50){
+    $("#feedback").text("Ice Cold");
+  }
+
+
+};
+
+function generateNumber(){
+  number = Math.floor((Math.random() * 100) + 1);
+};
 
 
 
-  /*-- Appending Number Guessed --*/
-    $('#guessButton').click(function(e) {
+
+
+$(document).ready(function(){
+
+  generateNumber();
+  console.log(number);
+  
+  /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
+
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
+
+    $("#guessButton").click(function(e){
       e.preventDefault();
-      var guess = $('#userGuess').val();
-      var list = '<li>' + guess + '</li>';
-      $('#guessList').append(list);
+      var guess = $("#userGuess").val();
+      var guessEl = '<li>' + guess + '</li>';
+      $('#guessList').append(guessEl);
       $('#userGuess').val('');
       incrementCounter();
+      checkGuess(guess);
     });
+
+  $(".new").on('click', function(){
+    window.location.reload(true);
+  });
+
+
+});
+
+
 
 
 });
